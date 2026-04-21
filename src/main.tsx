9e4a73d8d24f12e08ba4af4d24d16b70fa92bcbc
Kbilol <qochqorovbilol27@gmail.com>
2026-04-21 19:30:16 +0500

    Добавил информацию

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,37 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./app/index.css"
-import App from "./app/App.tsx"
+
 import { ThemeProvider } from "@/shared/providers/theme-provider.tsx"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import HomePage from "./Home page/Home.Page.tsx"
+import { ChatPage } from "./Chat page/Chat.Page.tsx"
+
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component:HomePage,
+  },
+  {
+    path:"/chat",
+    Component:ChatPage,
+  },
+  
+
+]);
+
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
+    
     </ThemeProvider>
   </StrictMode>
 )
